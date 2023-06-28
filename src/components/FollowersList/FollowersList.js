81102230a1fce0399eb93a68d14b397d97241dbf
José Antonio Velázquez Mandujano <jantonio.vema@gmail.com>
2023-06-28 15:37:21 -0600

    fix: Add AbortController for request

--- src/components/FollowersList/FollowersList.js
+++ src/components/FollowersList/FollowersList.js
@@ -8,19 +8,31 @@ export default function FollowersList() {
     const [followers, setFollowers] = useState([]);
 
     useEffect(() => {
-        fetchFollowers()
+        const abortController = new AbortController();
+        fetchFollowers(abortController.signal);
+        return () => {
+            abortController.abort();
+        };
     }, []);
 
-    const fetchFollowers = async () => {
-        const {data} = await axios.get("https://randomuser.me/api/?results=5")
-        setFollowers(data.results)
+    const fetchFollowers = async (signal) => {
+        try {
+            const {data} = await axios.get("https://randomuser.me/api/?results=5", { signal });
+            setFollowers(data.results);
+        } catch (error) {
+            if (error.name === 'AbortError') {
+                console.log('Fetch aborted');
+            } else {
+                throw error;
+            }
+        }
     }
 
     return (
-        <div className="followerslist-container">
+        <div className="followerslist-container" data-testid={"followerlist-container"}>
             <div>
-                {followers.map(follower => (
-                    <div className="follower-item">
+                {followers.map((follower, index) => (
+                    <div className="follower-item" key={index} data-testid={`follower-item-${index}`}>
                         <img src={follower.picture.large}/>
                         <div className="followers-details">
                             <div className="follower-item-name">
